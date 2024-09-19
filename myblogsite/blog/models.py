a0b11cc26de19f71282e40037a153c2f69ec4566
Anato-Eini <jameskennethacabal@gmail.com>
2024-09-19 21:58:21 +0800

    UserPost Model
    quit
    exit
    :w
    q

--- myblogsite/blog/models.py
+++ myblogsite/blog/models.py
@@ -1,23 +1,6 @@
 from django.db import models
 from django.contrib.auth.hashers import check_password
 
-class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    published_date = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return self.title
-
-class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    text = models.TextField()
-    author = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.author} on {self.post.title}"
-
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=255)
@@ -32,3 +15,26 @@ class User(models.Model):
 
     def check_password(self, raw_password):
         return check_password(raw_password, self.password)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class UserPost(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(UserPost, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.author} on {self.post.title}"
+
