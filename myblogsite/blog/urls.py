df800f1b3ea5f40d49b89d3b64256bf75473af22
Anato-Eini <jameskennethacabal@gmail.com>
2024-09-21 00:37:19 +0800

    Adjusted home path

--- myblogsite/blog/urls.py
+++ myblogsite/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.login_post, name='login'),
     path('register/', views.render_register, name='register'),
-    path('forums/', views.get_forums, name='home'),
+    path('home/', views.get_forums, name='home'),
     path('forums/<int:pk>', views.fetch_posts, name='posts'),
     path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
