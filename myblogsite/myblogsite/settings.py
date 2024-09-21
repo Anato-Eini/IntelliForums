0e3d2ba270701b0a099eccea8499b1160fc6f98c
Anato-Eini <jameskennethacabal@gmail.com>
2024-09-21 19:07:26 +0800

    Preparation for handling media

--- myblogsite/myblogsite/settings.py
+++ myblogsite/myblogsite/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for myblogsite project.
 
@@ -15,6 +16,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
