55c3992decc436144f6c42892b8b5b5bda6ea0ca
Anato-Eini <jameskennethacabal@gmail.com>
2024-09-21 13:15:21 +0800

    Admin Models for Models

--- myblogsite/blog/forms.py
+++ myblogsite/blog/forms.py
@@ -19,6 +19,31 @@ class LoginForm(forms.Form):
         label="Password"
     )
 
+class GeneralPost(forms.Form):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control'
+            }
+        ),
+        label="Enter title",
+        max_length=100
+    )
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control'
+            }
+        ),
+        label="Enter content",
+    )
+    choices = forms.ChoiceField(
+
+        choices=[ # (<value>, <label>)
+            ()
+        ]
+    )
+
 class RegisterForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
